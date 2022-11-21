81590338b2291fbecf6a3e5d455ee9c79767edff
d <d@dynamic>
2022-11-21 18:17:42 +0100

    Initial commit

---
+++ src/bootloader/stage2/headers/stdio.h
@@ -0,0 +1,7 @@
+#pragma once
+#include "stdint.h"
+
+void putc(char c);
+void puts(const char* str);
+void _cdecl printf(const char* fmt, ...);
+int* printf_number(int* argp, int length, bool sign, int radix);
