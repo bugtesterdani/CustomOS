8428b79e553dcccc79c5fc1cef6b976b8691adc6
daniel glasner <admin@daniprog.de>
2022-11-25 15:31:30 +0000

    Lösche alle bereits vorhandenen Dateien um das OS neu zu initialisieren

--- src/bootloader/stage2/headers/stdio.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-#include "stdint.h"
-
-void putc(char c);
-void puts(const char* str);
-void _cdecl printf(const char* fmt, ...);
-int* printf_number(int* argp, int length, bool sign, int radix);
