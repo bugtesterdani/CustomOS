753fc67be6795f340647ceac9bc8e3b59697ba47
d <d@dynamic>
2022-12-09 16:40:24 +0100

    * Output to Video Memory Strings * working Initial GDT added

--- src/bootloader/stage3/boot.asm
+++ src/bootloader/stage3/boot.asm
@@ -1,117 +1,137 @@
 bits 16
 ; org 0x8400
 
-%define ENDL                    0x0D, 0x0A
-; Stage1
-; 56 50 53 AC ...
-print_string:               dd 0x7D2A
-; BE xx 7D E8 09 00 B4 ...
-; BE xx 7C 01 E8 09 00 B4 ...
-wait_for_key_reboot:        dd 0x7D1B
-; F4 56 66 50 66 53 ...
-read_fat32:                 dd 0x7CC3
-; 10 00 02 00 ...
-DAP:                        dd 0x7CF5
-; 00 00 00 00 ...
-var_32_zeros:               dd 0x7DCE
+cli
+lgdt [gdt_desc]
+mov eax, cr0
+or al, 1
+mov cr0, eax
+jmp CODE_SEG:init
+jmp halt
 
-; Stage2
-ReadFile:                   dd 0x8077
-print_DAP_Values:           dd 0x8183
-print_hex_word:             dd 0x81BC
-set_in_dap_value:           dd 0x81DD
-
-
-init:
-    ; Is needed somewhy, to make the prints without bugs
-    push ax
-    pop ax
-    mov si, msg
-    call dword [print_string]
-    pop si
-    cli
-    call Activate_A20_line
-    call Load_GDT_Table
-    call Start_Protected_Mode
-
-hlt:
-    cli
+halt:
     hlt
-    jmp hlt
-
-msg:                        dd 'Hello World from Stage 3...', ENDL, 0
-
-
-; Following Code is in 32-bit
-; This is for the protected code
+    jmp halt
 
 bits 32
-Activate_A20_line:
-.check_A20_line:
-    pushad
-    mov edi, 0x112345               ; odd megabyte address.
-    mov esi, 0x012345               ; even megabyte address.
-    mov [esi], esi                  ; making sure that both addresses contain different values.
-    mov [edi], edi                  ; (if A20 line is cleared the two pointers would point to the address 0x012345 that would contain 0x112345 (edi))
-    cmpsd                           ; compare addresses to see if they are equivalent.
-    popad
-    jne .isActive
-    jmp .activate
+init:
+    mov ax, DATA_SEG
+    mov ds, ax
+    mov ss, ax
+    mov es, ax
+    mov fs, ax
+    mov gs, ax
+    mov ebp, 0x90000
+    mov esp, ebp
+    mov eax, 0                      ; Print to the 1. Line
+    mov si, print_string            ; Load the String we want to print
+    call print_to_video_card        ; Call the print function
+    mov eax, 1                      ; Print to the 2. Line
+    mov si, print_string_second
+    call print_to_video_card
+hang:
+    jmp hang
 
-.activate:
-    cli                             ; Disable Interrupts
-
-    call .a20wait
-    mov al, 0xAD
-    out 0x64, al
-
-    call .a20wait
-    mov al, 0xD0
-    out 0x64, al
-    
-    call a20wait2
-    in al, 0x60
-    push eax
-
-    call a20wait
-    mov al, 0xD1
-    out 0x64, al
-
-    call a20wait
-    pop eax
-    or al, 2
-    out 0x60, al
-    
-    call a20wait
-    mov al, 0xAE
-    out 0x64, al
-
-    call a20wait
-    sti                             ; Reset Interrupts
+bits 32
+print_to_video_card:
+    push si
+    mov ecx, 0x50
+    mul ecx
+    mov ecx, 2
+    mul ecx
+    mov edx, 0xB8000
+    add edx, eax
+    sub edx, 1
+.loop:
+    lodsb
+    or al, al
+    jz .done
+.print_char:
+    add edx, 1
+    mov byte [ds:edx], al
+    add edx, 1
+    mov byte [ds:edx], 0x1B
+    jmp .loop
+.done:
+    pop si
     ret
 
-.isActive:
-    ret
+%define ENDL 0x0D, 0x0A
 
-.a20wait:
-    in al, 0x64
-    test al, 2
-    jnz, .a20wait
-    ret
+print_string:           db 'Protected Mode', 0
+print_string_second:    db 'Welcome to DP OS', 0
 
-.a20wait2:
-    in al, 0x64
-    test al, 1
-    jz .a20wait2
-    ret
 
-Load_GDT_Table:
-    ret
 
-Start_Protected_Mode:
-    ; We need to modify the CR0 Register (Control Register 0)
-    mov eax, cr0
-    or al, 1        ; Set PE (Protection Enable) bit (lowest bit (2^0)) in CR0 (Control Register 0)
-    ; Then we need to store it at the Register back
-    mov cr0, eax
-    ret
+; Following the GDT Descriptions
+
+bits 16
+; Global Descriptor Tables
+;  1st Double word:
+;  Bits	            Function            Description
+;  0-15             Limit 0:15	        First 16 bits in the segment limiter
+;  16-31            Base 0:15	        First 16 bits in the base address
+;
+;
+;  2nd Double word:
+;  Bits	            Function            Description
+;  0-7              Base 16:23	        Bits 16-23 in the base address
+;  8-12             Type	            Segment type and attributes
+;  13-14            Privilege
+;           Level 0 = Highest privilege (OS), 
+;           Level 3 = Lowest privilege (User applications)
+;  15	            Present flag	    Set to 1 if segment is present
+;  16-19            Limit 16:19         Bits 16-19 in the segment limiter
+;  20-22            Attributes          Different attributes, depending on the segment type
+;  23	            Granularity
+;           Used together with the limiter, to determine the size of the segment
+;  24-31            Base 24:31          The last 24-31 bits in the base address
+
+; The GDT Descriptor
+;  Bits             Function            Description
+;  0-15             Limit               Size of GDT in bytes
+;  16-47            Address             GDT's memory address
+
+; Memory Map to 0xFFFFF
+;  Linear address range (hex)	Memory type	Use
+;  0 - 3FF          RAM                 Real mode, IVT (Interrupt Vector Table)
+;  400 - 4FF        RAM                 BDA (BIOS data area)
+;  500 - 9FFFF      RAM                 Free memory, 7C00 used for boot sector
+;  A0000 - BFFFF	Video RAM           Video memory
+;  C0000 - C7FFF	Video ROM           Video BIOS
+;  C8000 - EFFFF	?                   BIOS shadow area
+;  F0000 - FFFFF    ROM                 System BIOS
+
+gdt_start:
+
+gdt_null:
+    dq 0
+
+gdt_code:
+    ; 1. Double word
+    dw 0FFFFh           ; 4GB Limit
+    dw 0                ; Offset / Base Address set to 0
+    ; 2. Double word
+    db 0                ; Base Address 8 16-23 bit
+    db 10011010b
+    db 11001111b
+    db 0
+
+gdt_data:
+    ; 1. Double word
+    dw 0FFFFh           ; 4GB Limit
+    dw 0                ; Offset / Base Address set to 0
+    ; 2. Double word
+    db 0                ; Base Address 8 16-23 bit
+    db 10010010b
+    db 11001111b
+    db 0
+
+gdt_end:
+
+gdt_desc:
+    dw gdt_end - gdt_start - 1
+    dd gdt_start
+
+CODE_SEG equ gdt_code - gdt_start
+DATA_SEG equ gdt_data - gdt_start
