e5981fe46d695a05f0bd28f37fa981223126aba5
d <d@dynamic>
2022-12-30 21:23:20 +0100

    made some maybe usefull changes

--- make.sh
+++ make.sh
@@ -15,6 +15,8 @@ sudo docker cp compile_runner:/root/src/build/disk.img build/disk.img
 # sudo docker cp compile_runner:/root/src/bootloader/stage3/build/stage3.bin build/stage3.bin
 sudo chown $USER:$USER build/disk.img
 
+# sudo docker exec -it compile_runner /bin/bash
+
 sudo docker stop compile_runner
 
 sudo docker rm compile_runner
@@ -25,8 +27,8 @@ vboxmanage convertfromraw --format VDI --uuid=220868a3-515d-4693-875f-491f7bf306
 chmod +w build/disk.vdi
 
 # Start debugging Instance
-qemu-system-x86_64 -s -S -m 4096 -hda build/disk.img &
-# qemu-system-i386 -s -S -hda build/disk.img &
+# qemu-system-x86_64 -s -S -m 4096 -hda build/disk.img &
+qemu-system-i386 -s -S -hda build/disk.img &
 #gdb -ix "gdb_init_real_mode.txt" build/stage3.elf -ex "target remote localhost:1234" -ex "br *0x7c00" -ex "br *0x8000" -ex "br *0x8400" -ex "c"
-gdb -ix "gdb_init_test.txt" -ex "target remote localhost:1234" -ex "br *0x8459" -ex "c"
-# gdb -ix "gdb_init_real_mode.txt" -ex "target remote localhost:1234" -ex "br *0x8400" -ex "c"
+# gdb -ix "gdb_init_test.txt" -ex "target remote localhost:1234" -ex "br *0x8400" -ex "c"
+gdb -ix "gdb_init_real_mode.txt" -ex "target remote localhost:1234" -ex "br *0x8400" -ex "c"
--- src/builder.sh
+++ src/builder.sh
@@ -13,10 +13,24 @@ objcopy -O binary build/stage1.elf build/boot.bin
 
 # Build Bootloader Stage 2
 nasm -f elf32 -g3 -F dwarf build_asmparser/stage2.asm -o build/stage2.o
-ld -Ttext=0x8000 -melf_i386 build/stage2.o -o build/stage2.elf
+ld -Ttext=0x8400 -melf_i386 build/stage2.o -o build/stage2.elf
 objcopy -O binary build/stage2.elf build/stage2.bin
 
+# ./asmparser bootloader/stage3/stage3.asm build/ build_asmparser/
+
+# Build Bootloader Stage 3
+# nasm -f elf32 -g3 -F dwarf build_asmparser/stage3.asm -o build/stage3.o
+# ld -Ttext=0x8400 -melf_i386 build/stage3.o -o build/stage3.elf
+# objcopy -O binary build/stage3.elf build/stage3.bin
+
+# rm bootloader/stage3/stage3.asm
+# cp build_asmparser/stage3.asm bootloader/stage3/stage3.asm
+
 cd bootloader/stage3
+make
+cd ../..
+
+cd bootloader/stage4
 make
 cd ../..
 
@@ -26,6 +40,8 @@ dd if=/dev/zero of=disk.img bs=512 count=1440000
 mformat -F -i disk.img ::
 mcopy -i disk.img stage2.bin ::
 mcopy -i disk.img stage2.bin ::/stage22.bin
+# mcopy -i disk.img stage3.bin ::
 mcopy -i disk.img ../bootloader/stage3/build/stage3.bin ::
+mcopy -i disk.img ../bootloader/stage4/build/stage4.bin ::
 # mcopy -i disk.img stage4.bin ::
 dd if=boot.bin of=disk.img conv=notrunc
